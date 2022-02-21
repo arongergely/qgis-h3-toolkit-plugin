16327c84351299c17b91e6d58349d6466bb0c85c
Aron Gergely <aron.gergely@rasterra.nl>
2022-02-21 21:06:23 +0100

    initial commit

---
+++ __init__.py
@@ -0,0 +1,35 @@
+#-----------------------------------------------------------
+# Copyright (C) 2022 Aron Gergely
+#-----------------------------------------------------------
+# Licensed under the terms of GNU GPL 2
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#---------------------------------------------------------------------
+import os
+
+from qgis.core import QgsApplication
+from PyQt5.QtWidgets import QAction, QMessageBox
+
+from .provider import H3Provider
+
+
+def classFactory(iface):
+    return H3Toolbox()
+
+
+class H3Toolbox:
+    def __init__(self):
+        self.provider = None
+
+    def initProcessing(self):
+        self.provider = H3Provider()
+        QgsApplication.processingRegistry().addProvider(self.provider)
+
+    def initGui(self):
+        self.initProcessing()
+
+    def unload(self):
+        QgsApplication.processingRegistry().removeProvider(self.provider)
---
+++ provider.py
@@ -0,0 +1,20 @@
+from qgis.core import QgsProcessingProvider
+from qgis.PyQt.QtGui import QIcon
+
+from .create_h3_grid import CreateH3GridProcessingAlgorithm
+
+
+class H3Provider(QgsProcessingProvider):
+    def loadAlgorithms(self, *args, **kwargs):
+        self.addAlgorithm(CreateH3GridProcessingAlgorithm())
+        # add additional algorithms here
+        # self.addAlgorithm(MyOtherAlgorithm())
+
+    def id(self, *args, **kwargs):
+        return 'H3'
+
+    def name(self, *args, **kwargs):
+        return 'H3'
+
+    def icon(self):
+        return QIcon()#QIcon(os.path.join(os.path.dirname(__file__), 'h3_logo.svg'))
