d11429164695dae1bc915345851e799f6fa079c3
Aron Gergely <aron.gergely@rasterra.nl>
2025-06-11 11:04:41 +0200

    refactor:  inline the h3 lib checker function

--- h3_toolkit/h3_dependency_guard.py
+++ h3_toolkit/h3_dependency_guard.py
@@ -1,13 +1,6 @@
 from importlib.util import find_spec
 from importlib.metadata import version
 
-def _isModuleImportable(module_name):
-    """Verify whether or not a module can be imported."""
-    
-    module_spec = find_spec(module_name)
-    
-    return bool(module_spec)
 
-
-IS_H3_PRESENT = _isModuleImportable("h3")
+IS_H3_PRESENT = bool(find_spec("h3"))
 H3_VERSION = version("h3") if IS_H3_PRESENT else None
